123443cbab16b63879711a9cfbc59d34ba77b2f3
Eduardo Ferraz <edupferraz@gmail.com>
2023-03-08 07:36:56 -0300

    Images in Next

--- ignite-shop/next.config.js
+++ ignite-shop/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  setMinify: true,
+
+  experimental: {
+    images: {
+      allowFutureImage: true
+    }
+  }
 }
 
 module.exports = nextConfig
