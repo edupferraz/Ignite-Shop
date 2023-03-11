a3ba16d634df9a76f05fc127dc0b93cb60d8f9f5
Eduardo Ferraz <edupferraz@gmail.com>
2023-03-11 15:37:50 -0300

    Product Page Structure

--- ignite-shop/src/pages/product/[id].tsx
+++ ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,26 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
 
-    return <h1>Product: {JSON.stringify(query)}</h1>
+    return (
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 69,90</span>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus molestiae quas aperiam praesentium iusto, nemo ullam neque amet rerum quam tenetur perspiciatis? Incidunt at quos, aliquid dolorem nemo labore perspiciatis!</p>
+
+                <button>Compra agora</button>
+
+            </ProductDetails>
+        </ProductContainer>
+
+
+    )
 }
